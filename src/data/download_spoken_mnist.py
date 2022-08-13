e5025c4d6284ab7d4ab65f6dc3211fd3e67e035d
Krzysztof Buzar <kbuzar@gmail.com>
2022-08-13 22:29:48 +0200

    Moved Spoken MNIST download script

--- src/data/download_spoken_mnist.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-
-def download_spoken_mnist():
-    pass
---
+++ src/utils/download_spoken_mnist.py
@@ -0,0 +1,9 @@
+import requests
+
+"""
+download spoken mnist, either directly as wav files or as a tensorflow dataset object (both have consequences later)
+"""
+
+
+def download_spoken_mnist():
+    pass
